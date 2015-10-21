790dec4397b3298515f2c9ae44f3bc035a65361f
Lucas Atayde <dfwlucas@gmail.com>
2015-10-20 20:04:21 -0500

    onActivityResult added. Pick up at device discovery http://toastdroid.com/2014/09/22/android-bluetooth-low-energy-tutorial/#more-189 http://blog.davidvassallo.me/2015/09/02/ble-health-devices-first-steps-with-android/

--- app/src/main/java/com/heartbeat/watafuru/heartbeat/Settings.java
+++ app/src/main/java/com/heartbeat/watafuru/heartbeat/Settings.java
@@ -1,44 +1,34 @@
 package com.heartbeat.watafuru.heartbeat;
-
+import android.app.Activity;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.Set;
-import java.util.UUID;
+import android.widget.ListView;
+
 
 public class Settings extends ListActivity {
 
 
 
     private static final String TAG = "";
+    BluetoothManager btManager;
     BluetoothAdapter mBluetoothAdapter;
-    UUID mUUID;
-    BluetoothSocket mSocket;
+    private int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
+        btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = btManager.getAdapter();
 
         if (mBluetoothAdapter == null) {
             //TODO: add a "I need bluetooth message"
@@ -81,6 +71,13 @@ public class Settings extends ListActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Bluetooth not enabled.
+               return;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
 
     }
 }
